6458e0070855f92652972260673d3ba909c424cc
tpemartin <tpemartin@gmail.com>
2022-05-25 15:47:55 +0800

    Create may 25.R

---
+++ may 25.R
@@ -0,0 +1,49 @@
+source("https://raw.githubusercontent.com/tpemartin/110-2-R/main/support/weather.R", encoding="UTF-8") # R 4.2+
+source("https://raw.githubusercontent.com/tpemartin/110-2-R/main/traffic_accident.R", encoding="UTF-8")
+
+weather = Weather()
+weather$dowload_data()
+
+
+
+whichStation = 5
+whichDateTimeRow = 6090
+whichDateRow = 254
+
+climateDataList <- vector("list", nrow(traffic))
+for(.x in 1:nrow(traffic)){
+  .x = 1
+  traffic[.x, ] -> trafficX
+  
+  # trafficX$經度 trafficX$緯度 ==> whichStation
+  trafficX ==> whichDateTimeRow
+  trafficX ==> whichDateRow
+  
+  weather[["rawData"]][["cwbdata"]][["resources"]][["resource"]][["data"]][["surfaceObs"]][["location"]][["station"]][whichStation,]
+  
+  climatedata1 <- list()
+  #從中發現竹子湖是第五筆資料 觀察其他氣象資料皆有對稱性 
+  weather[["rawData"]][["cwbdata"]][["resources"]][["resource"]][["data"]][["surfaceObs"]][["location"]][["stationObsTimes"]][["stationObsTime"]][[whichStation]][whichDateTimeRow,1]->climatedata1$date
+  weather[["rawData"]][["cwbdata"]][["resources"]][["resource"]][["data"]][["surfaceObs"]][["location"]][["stationObsTimes"]][["stationObsTime"]][[whichStation]][whichDateTimeRow,2]->climatedata1$stationPressure
+  weather[["rawData"]][["cwbdata"]][["resources"]][["resource"]][["data"]][["surfaceObs"]][["location"]][["stationObsStatistics"]][["temperature"]][["daily"]][[whichStation]][whichDateRow,2:4]->climatedata1$temperature
+  
+  climateDataList[[.x]] <- climatedata1
+}
+
+
+
+# For each accident -------------------------------------------------------
+
+traffic |> head()
+
+
+
+
+
+
+
+
+
+
+
+
